181d05c2db6b2691f889aad7b2f3f739e13b517a
Andrew de Quincey <adq@lidskialf.net>
2017-08-06 19:16:40 +0100

    disable setting of device name

--- adq/ble.c
+++ adq/ble.c
@@ -18,11 +18,11 @@ static void gap_params_init(void)
 {
     uint32_t                err_code;
     ble_gap_conn_params_t   gap_conn_params;
-    ble_gap_conn_sec_mode_t sec_mode;
+    ble_gap_conn_sec_mode_t w_sec_mode;
 
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
+    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&w_sec_mode);
 
-    err_code = sd_ble_gap_device_name_set(&sec_mode,
+    err_code = sd_ble_gap_device_name_set(&w_sec_mode,
                                           (const uint8_t *)DEVICE_NAME,
                                           strlen(DEVICE_NAME));
     APP_ERROR_CHECK(err_code);
