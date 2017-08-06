3e8d8f8ce9e14a623eec1e7e7b7cabc0fc5af66c
Andrew de Quincey <adq@lidskialf.net>
2017-08-06 19:04:36 +0100

    cleanupp

--- adq/motor.c
+++ adq/motor.c
@@ -2,13 +2,19 @@
 #include "i5plus.h"
 
 void motor_init() {
-  nrf_gpio_cfg_output(GPIO_MOTOR);
+  NRF_GPIO->PIN_CNF[GPIO_MOTOR] = (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos) \
+                                 |(GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos)    \
+                                 |(GPIO_PIN_CNF_PULL_Pulldown << GPIO_PIN_CNF_PULL_Pos)  \
+                                 |(GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos) \
+                                 |(GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos);
 }
 
 void motor_on() {
   nrf_gpio_pin_set(GPIO_MOTOR);
+  nrf_gpio_pin_dir_set(GPIO_MOTOR, NRF_GPIO_PIN_DIR_OUTPUT);
 }
 
 void motor_off() {
+  nrf_gpio_pin_dir_set(GPIO_MOTOR, NRF_GPIO_PIN_DIR_INPUT);
   nrf_gpio_pin_clear(GPIO_MOTOR);
 }
