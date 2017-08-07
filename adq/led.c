9bb51e74647fef4f9f151d8217081ccb316d3073
Andrew de Quincey <adq@lidskialf.net>
2017-08-07 01:08:58 +0100

    gpio cleanup

--- adq/led.c
+++ adq/led.c
@@ -2,19 +2,18 @@
 #include "i5plus.h"
 
 void led_init() {
+  nrf_gpio_pin_clear(GPIO_LED);
   NRF_GPIO->PIN_CNF[GPIO_LED] = (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos) \
                                  |(GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos)    \
-                                 |(GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos)  \
-                                 |(GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos) \
-                                 |(GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos);
+                                 |(GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)  \
+                                 |(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos) \
+                                 |(GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos);
 }
 
 void led_on() {
   nrf_gpio_pin_set(GPIO_LED);
-  nrf_gpio_pin_dir_set(GPIO_LED, NRF_GPIO_PIN_DIR_OUTPUT);
 }
 
 void led_off() {
-  nrf_gpio_pin_dir_set(GPIO_LED, NRF_GPIO_PIN_DIR_INPUT);
   nrf_gpio_pin_clear(GPIO_LED);
 }
