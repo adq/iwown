a12f1baf34747ca0dc2fc4f52f791de4aff96a2c
Andrew de Quincey <adq@lidskialf.net>
2017-08-27 20:04:30 +0100

    small cleanups

--- adq/OLED_SSD1306.c
+++ adq/OLED_SSD1306.c
@@ -31,6 +31,50 @@ All text above, and the splash screen below must be included in any redistributi
 #include "simple_uart.h"
 
 
+#define SSD1306_SETCONTRAST 0x81
+#define SSD1306_DISPLAYALLON_RESUME 0xA4
+#define SSD1306_DISPLAYALLON 0xA5
+#define SSD1306_NORMALDISPLAY 0xA6
+#define SSD1306_INVERTDISPLAY 0xA7
+#define SSD1306_DISPLAYOFF 0xAE
+#define SSD1306_DISPLAYON 0xAF
+
+#define SSD1306_SETDISPLAYOFFSET 0xD3
+#define SSD1306_SETCOMPINS 0xDA
+
+#define SSD1306_SETVCOMDETECT 0xDB
+
+#define SSD1306_SETDISPLAYCLOCKDIV 0xD5
+#define SSD1306_SETPRECHARGE 0xD9
+
+#define SSD1306_SETMULTIPLEX 0xA8
+
+#define SSD1306_SETLOWCOLUMN 0x00
+#define SSD1306_SETHIGHCOLUMN 0x10
+
+#define SSD1306_SETSTARTLINE 0x40
+
+#define SSD1306_MEMORYMODE 0x20
+#define SSD1306_COLUMNADDR 0x21
+#define SSD1306_PAGEADDR   0x22
+
+#define SSD1306_COMSCANINC 0xC0
+#define SSD1306_COMSCANDEC 0xC8
+
+#define SSD1306_SEGREMAP 0xA0
+
+#define SSD1306_CHARGEPUMP 0x8D
+
+// Scrolling #defines
+#define SSD1306_ACTIVATE_SCROLL 0x2F
+#define SSD1306_DEACTIVATE_SCROLL 0x2E
+#define SSD1306_SET_VERTICAL_SCROLL_AREA 0xA3
+#define SSD1306_RIGHT_HORIZONTAL_SCROLL 0x26
+#define SSD1306_LEFT_HORIZONTAL_SCROLL 0x27
+#define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29
+#define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL 0x2A
+
+
 // the memory buffer for the LCD
 
 static uint8_t buffer[SSD1306_LCDHEIGHT * SSD1306_LCDWIDTH / 8] = {
@@ -109,6 +153,7 @@ static uint8_t buffer[SSD1306_LCDHEIGHT * SSD1306_LCDWIDTH / 8] = {
 
 static void OLED_drawFastVLineInternal(int16_t x, int16_t __y, int16_t __h, uint16_t color);
 static void OLED_drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color);
+static void OLED_command(uint8_t c);
 
 static twi_master_config_t twi_oled = {TWI_Pin_SCL:GPIO_OLED_SCL, TWI_Pin_SDA:GPIO_OLED_SDA};
 
@@ -228,7 +273,7 @@ void OLED_invertDisplay(bool i) {
   }
 }
 
-void OLED_command(uint8_t c) {
+static void OLED_command(uint8_t c) {
   uint8_t txbuf[2];
   txbuf[0] = 0x00;  // control byte
   txbuf[1] = c;
--- adq/OLED_SSD1306.h
+++ adq/OLED_SSD1306.h
@@ -43,8 +43,6 @@ All text above, and the splash screen must be included in any redistribution
 //   #define SSD1306_96_16
 /*=========================================================================*/
 
-#define SSD1306_EXTERNALVCC
-
 #if defined SSD1306_128_64 && defined SSD1306_128_32
   #error "Only one SSD1306 display can be specified at once in SSD1306.h"
 #endif
@@ -65,63 +63,17 @@ All text above, and the splash screen must be included in any redistribution
   #define SSD1306_LCDHEIGHT                 16
 #endif
 
-#define SSD1306_SETCONTRAST 0x81
-#define SSD1306_DISPLAYALLON_RESUME 0xA4
-#define SSD1306_DISPLAYALLON 0xA5
-#define SSD1306_NORMALDISPLAY 0xA6
-#define SSD1306_INVERTDISPLAY 0xA7
-#define SSD1306_DISPLAYOFF 0xAE
-#define SSD1306_DISPLAYON 0xAF
-
-#define SSD1306_SETDISPLAYOFFSET 0xD3
-#define SSD1306_SETCOMPINS 0xDA
-
-#define SSD1306_SETVCOMDETECT 0xDB
-
-#define SSD1306_SETDISPLAYCLOCKDIV 0xD5
-#define SSD1306_SETPRECHARGE 0xD9
-
-#define SSD1306_SETMULTIPLEX 0xA8
-
-#define SSD1306_SETLOWCOLUMN 0x00
-#define SSD1306_SETHIGHCOLUMN 0x10
-
-#define SSD1306_SETSTARTLINE 0x40
-
-#define SSD1306_MEMORYMODE 0x20
-#define SSD1306_COLUMNADDR 0x21
-#define SSD1306_PAGEADDR   0x22
-
-#define SSD1306_COMSCANINC 0xC0
-#define SSD1306_COMSCANDEC 0xC8
-
-#define SSD1306_SEGREMAP 0xA0
-
-#define SSD1306_CHARGEPUMP 0x8D
-
-// Scrolling #defines
-#define SSD1306_ACTIVATE_SCROLL 0x2F
-#define SSD1306_DEACTIVATE_SCROLL 0x2E
-#define SSD1306_SET_VERTICAL_SCROLL_AREA 0xA3
-#define SSD1306_RIGHT_HORIZONTAL_SCROLL 0x26
-#define SSD1306_LEFT_HORIZONTAL_SCROLL 0x27
-#define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29
-#define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL 0x2A
-
 extern void OLED_init();
-extern void OLED_command(uint8_t c);
+extern void OLED_on();
+extern void OLED_off();
 
 extern void OLED_clearDisplay();
 extern void OLED_updateDisplay();
 
 extern void OLED_startscrollright(uint8_t start, uint8_t stop);
 extern void OLED_startscrollleft(uint8_t start, uint8_t stop);
-
 extern void OLED_startscrolldiagright(uint8_t start, uint8_t stop);
 extern void OLED_startscrolldiagleft(uint8_t start, uint8_t stop);
 extern void OLED_stopscroll();
-
-extern void OLED_on();
-extern void OLED_off();
 
 #endif /* _OLED__SSD1306_H_ */
