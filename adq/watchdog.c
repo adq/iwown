57137cfe74989cd5b34c1ca9c87bb5fb2c31f0ea
Andrew de Quincey <adq@tvsquared.com>
2017-08-29 12:48:19 +0100

    zap debug

--- adq/watchdog.c
+++ adq/watchdog.c
@@ -1,14 +1,11 @@
 #include "nrf_gpio.h"
 #include "nrf.h"
 #include "i5plus.h"
-#include "simple_uart.h"
 
 void watchdog_init() {
     NRF_WDT->RREN = 1;
 }
 
 void watchdog_reload() {
-    simple_uart_putstring((uint8_t*) "WDT RELOAD\r\n");
-
     NRF_WDT->RR[0] = WDT_RR_RR_Reload;
 }
