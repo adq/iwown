2d2187830b07b5a3862868666b43ddf099f05ca1
Andrew de Quincey <adq@lidskialf.net>
2017-08-30 23:37:06 +0100

    setting up battery level sampling code

--- adq/power.c
+++ adq/power.c
@@ -2,9 +2,18 @@
 #include "nrf.h"
 #include "i5plus.h"
 #include "power.h"
+#include "softdevice_handler.h"
 
+/**@brief Macro to convert the result of ADC conversion in millivolts.
+ *
+ * @param[in]  ADC_VALUE   ADC result.
+ * @retval     Result converted to millivolts.
+ */
+ #define ADC_RESULT_IN_MILLI_VOLTS(ADC_VALUE) ((((ADC_VALUE) * 1200) >> 10) * 12000)
 
 void power_init() {
+    uint32_t err_code;
+
     NRF_GPIO->PIN_CNF[GPIO_USB_POWER] = (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos) \
                                        |(GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos)    \
                                        |(GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)  \
@@ -12,6 +21,65 @@ void power_init() {
                                        |(GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos);
 }
 
+void power_dosample() {
+    // Configure ADC
+    NRF_ADC->INTENSET   = ADC_INTENSET_END_Msk;
+    NRF_ADC->CONFIG     = (ADC_CONFIG_RES_10bit                       << ADC_CONFIG_RES_Pos)     |
+                          (ADC_CONFIG_INPSEL_AnalogInputNoPrescaling  << ADC_CONFIG_INPSEL_Pos)  |
+                          (ADC_CONFIG_REFSEL_VBG                      << ADC_CONFIG_REFSEL_Pos)  |
+                          (ADC_CONFIG_PSEL_AnalogInput7               << ADC_CONFIG_PSEL_Pos)    |
+                          (ADC_CONFIG_EXTREFSEL_None                  << ADC_CONFIG_EXTREFSEL_Pos);
+    NRF_ADC->EVENTS_END = 0;
+    NRF_ADC->ENABLE     = ADC_ENABLE_ENABLE_Enabled;
+
+    // Enable ADC interrupt
+    err_code = sd_nvic_ClearPendingIRQ(ADC_IRQn);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = sd_nvic_SetPriority(ADC_IRQn, NRF_APP_PRIORITY_LOW);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = sd_nvic_EnableIRQ(ADC_IRQn);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_ADC->EVENTS_END  = 0;    // Stop any running conversions.
+    NRF_ADC->TASKS_START = 1;
+}
+
 uint8_t power_readusbstatus() {
     return ((NRF_GPIO->IN & GPIO_USB_POWER) ? 1 : 0);
+}
+
+void ADC_IRQHandler(void)
+{
+    if (NRF_ADC->EVENTS_END != 0)
+    {
+        uint8_t     adc_result;
+        uint16_t    batt_lvl_in_milli_volts;
+        uint8_t     percentage_batt_lvl;
+        uint32_t    err_code;
+
+        NRF_ADC->EVENTS_END     = 0;
+        adc_result              = NRF_ADC->RESULT;
+        NRF_ADC->TASKS_STOP     = 1;
+
+        batt_lvl_in_milli_volts = ADC_RESULT_IN_MILLI_VOLTS(adc_result);
+        percentage_batt_lvl     = battery_level_in_percent(batt_lvl_in_milli_volts);
+
+        /*
+        err_code = ble_bas_battery_level_update(&m_bas, percentage_batt_lvl);
+        if (
+            (err_code != NRF_SUCCESS)
+            &&
+            (err_code != NRF_ERROR_INVALID_STATE)
+            &&
+            (err_code != BLE_ERROR_NO_TX_BUFFERS)
+            &&
+            (err_code != BLE_ERROR_GATTS_SYS_ATTR_MISSING)
+        )
+        {
+            APP_ERROR_HANDLER(err_code);
+        }
+        */
+    }
 }
